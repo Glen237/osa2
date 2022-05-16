cae9fde9ea1eaf4ec93e8918424a1c21b5b85c0e
glen <nmfombe@yahoo.com>
2022-05-16 19:15:19 +0300

    added total in exercices

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -1,15 +1,20 @@
 import React from 'react'
 
 const Contents = ({contents}) => {
-    
+
+   let total = 0;
+   
   return (
     <div>
         <h2>Contents</h2>
         <ul>
 {contents.map((course)=>{
-    return <li key={course.id}>{course.name}</li>
+    total = total + course.exercises;
+    return <li key={course.id}>{course.name + " " + course.exercises}</li>
 })}
+<p>Total {total}</p>
         </ul>
+
     </div>
   )
 }
