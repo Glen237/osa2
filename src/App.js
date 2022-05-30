518166f5b664a844b6b8a26fe559d19358f26570
glen <nmfombe@yahoo.com>
2022-05-30 18:32:02 +0300

    loading data done

--- src/App.js
+++ src/App.js
@@ -1,49 +1,74 @@
 import React from 'react';
+import axios from 'axios'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      persons: [
-        { name: 'Arto Hellas' ,phone:"123-304"}
-      ],
+      persons: [],
       newName: '',
       newPhoneNumber:''
     }
   }
 
+  componentDidMount(){
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      this.setState({persons:response.data})
+    })
+  }
+  handleSubmit = (event)=>{
+    event.preventDefault();
+    
+let nameExists = false;
+
+for(let i=0;i<this.state.persons.length;i++){
+if(this.state.persons[i].name === this.state.newName){
+  nameExists = true;
+}
+}
+if(nameExists==true){
+alert("name already exists")
+
+}
+else { 
+  const person = {
+  name: this.state.newName,
+  number: this.state.newPhoneNumber
+}
+
+axios.post('http://localhost:3001/persons', person)
+  .then(response => {
+    this.setState({persons:this.state.persons.concat(response.data)})
+    this.setState({newName:""});
+this.setState({newPhoneNumber:""})
+  })
+}
+  }
+
+  delete(id){
+    console.log(id)
+    if (!window.confirm("Do you really want to delete?")) {
+      return;
+    }
+    axios.delete (`http://localhost:3001/persons/${id}`)
+    .then(response =>{ this.setState({persons:this.state.persons.filter((item) => item.id!==id)})
+
+    })
+  }
   render() {
+    console.log(this.state)
     const handleNameChange =(event)=>{
 this.setState({newName:event.target.value})
     }
     const handlePhoneNumberChange =(event)=>{
       this.setState({newPhoneNumber:event.target.value})
           }
-    const handleSubmit = (event)=>{
-      event.preventDefault();
-      
-let nameExists = false;
-
-for(let i=0;i<this.state.persons.length;i++){
-  if(this.state.persons[i].name === this.state.newName){
-    nameExists = true;
-  }
-}
-if(nameExists==true){
-  alert("name already exists")
-  
-}else{
-  this.state.persons = [...this.state.persons,{name:this.state.newName,phone:this.state.newPhoneNumber}]
-}
-
-this.setState({newName:""});
-this.setState({newPhoneNumber:""})
-    }
-    
     return (
       <div>
         <h2>Puhelinluettelo</h2>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={this.handleSubmit}>
           <div>
             nimi: <input onChange={handleNameChange} value={this.state.newName}/>
             </div>
@@ -57,7 +82,8 @@ this.setState({newPhoneNumber:""})
         <h2>Numerot</h2>
         <ul>
          {this.state.persons.map(person=>{
-           return <li key={person.name}>{person.name + " " + person.phone}</li>
+           return <li key={person.name}>{person.name + " " + person.number} 
+           <button onClick={() => this.delete(person.id)}>Delete</button></li>
          })}
         </ul>
       </div>
